38ba7e9a7b604860d24218cb5315aded450d9f83
Belrah <belrahj3@gmail.com>
2022-03-23 20:52:47 +0100

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
+
   int n;
   int a[5];
   int *p;
 
   a[2] = 1024;
   p = &n;
+  
   /*
    * write your line of code here...
    * Remember:
@@ -16,6 +18,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
+  
   p[5] = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
